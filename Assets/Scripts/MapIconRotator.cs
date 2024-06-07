2b675dc43b949a1854aed9e02777b4e798a06f70
BathtimeBill <80926404+BathtimeBill@users.noreply.github.com>
2024-06-07 12:25:00 +1000

    Map Icons Rotate With Map

--- Assets/Scripts/MapIconRotator.cs
+++ Assets/Scripts/MapIconRotator.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class MapIconRotator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject playerCam;
+    public float targetYRotation;
+    public Quaternion targetRotation;
+    public float targetScale;
+
+
+    private void Start()
     {
-        
+        playerCam = GameObject.FindGameObjectWithTag("PlayerCam");
+        targetScale = transform.localScale.z - transform.localScale.z*2;
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, targetScale);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        transform.rotation = Quaternion.Euler(0, 180, 0);
+        targetRotation = Quaternion.Euler(0, playerCam.transform.localEulerAngles.y, 0);
+        transform.rotation = targetRotation;
     }
+
 }
